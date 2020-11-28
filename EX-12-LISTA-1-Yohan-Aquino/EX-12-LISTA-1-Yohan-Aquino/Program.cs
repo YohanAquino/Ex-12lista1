02209430f216087f2aea80cea82564d7ea5216ce
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:40:19 -0300

    Add files via upload

---
+++ EX-12-LISTA-1-Yohan-Aquino/EX-12-LISTA-1-Yohan-Aquino/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_12_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*13. Verificar se três valores quaisquer (A,B, C) que serão digitados formam ou não um
+triângulo retângulo. Lembre-se que o quadrado da hipotenusa é igual a soma dos quadrados dos
+catetos. */
+
+            Triangulo tri = new Triangulo();
+
+            Console.WriteLine("Digite o 1°valor");
+            tri.setNum1(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o 2°valor");
+            tri.setNum2(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o 3°valor");
+            tri.setNum3(int.Parse(Console.ReadLine()));
+
+            if (tri.verificarTriangulo())
+            {
+                Console.WriteLine("Forma um triângulo");
+            }
+            else
+            {
+                Console.WriteLine("Não forma um triângulo");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EX-12-LISTA-1-Yohan-Aquino/EX-12-LISTA-1-Yohan-Aquino/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_12_LISTA_1_Yohan_Aquino
+{
+    class Triangulo
+    {
+        private int n1;
+        private int n2;
+        private int n3;
+        private double r1;
+        private double r2;
+        private double r3;
+        public void setNum1(int x)
+        {
+            n1 = x;
+        }
+        public void setNum2(int x)
+        {
+            n2 = x;
+        }
+        public void setNum3(int x)
+        {
+            n3 = x;
+        }
+        public Boolean verificarTriangulo()
+        {
+            r1 = Math.Sqrt(Math.Pow(n2, 2) + Math.Pow(n3, 2));
+            r2 = Math.Sqrt(Math.Pow(n1, 2) + Math.Pow(n3, 2));
+            r3 = Math.Sqrt(Math.Pow(n1, 2) + Math.Pow(n2, 2));
+            if (n1 == r1  || n2 == r2 || n3 == r3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
